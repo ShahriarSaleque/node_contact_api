c4e9f3517ee8b3aaccfd7ec1690abb8a92b70478
ShahriarSaleque <salequeshahriar@gmail.com>
2020-02-17 19:56:54 +0600

    routes doing

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,18 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 const mongoose = require("mongoose");
 
 //Bring in the Schema
 const Contact = require("../model/Contact");
+
+//POST request
+router.post("/", (req, res) => {
+  res.send("POSt");
+});
+
+//GET request
+router.get("/", (req, res) => {
+  res.send("GET");
+});
+
+module.exports = router;
